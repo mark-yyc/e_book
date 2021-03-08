867d0c844e675e21c1ae094fa32dc572b4cb9038
myname <mymail>
2021-03-08 23:48:48 +0800

    fix: pom.xml fix

--- bookstore_backend/rmi_server/src/main/java/com/mark/rmi/RmiApplication.java
+++ bookstore_backend/rmi_server/src/main/java/com/mark/rmi/RmiApplication.java
@@ -2,12 +2,10 @@ package com.mark.rmi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds= 1800)
 public class RmiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BookstoreApplication.class, args);
+        SpringApplication.run(RmiApplication.class, args);
     }
 }
